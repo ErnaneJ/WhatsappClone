6b5af946a1cb8a363424a321fb2f5e81d86ee4db
Ernane Ferreira <ernane.junior25@gmail.com>
2021-10-23 01:26:41 -0300

    Implementando interface da home - landing page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Api from '../../db/Api.js';
+import './Login.css';
+
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 
 export default function Login({onReceive}){
+
+  const [loginOptions, setLoginOptions] = useState(false);
+
+  const handleLoginOption = () => setLoginOptions(!loginOptions);
+
   const handleFacebookLogin = async ()=>{
     let result = await Api.FacebookPopup();
     if(result){onReceive(result.user)}
@@ -18,10 +26,34 @@ export default function Login({onReceive}){
     else{console.error('Erro!')}
   }
   return (
-    <div className="login">
-      <button onClick={handleFacebookLogin}>Logar com o Facebook</button>
-      <button onClick={handleGithubLogin}>Logar com o Github</button>
-      <button onClick={handleGoogleLogin}>Logar com o Google</button>
-    </div>
+    <section className="container-login">
+      <div className="login-topbar">
+        <div className="topbar--version">2021 - Web App</div>
+        <div className="topbar--version">
+          <div className="topbar--login" onClick={handleLoginOption}>
+            <div>Login</div> 
+            <KeyboardArrowDownIcon style={{color: '#ccc', transform: `rotate(${loginOptions ? '180deg' : '0'})`}}/>
+          </div>
+          <div className="login-options" style={{display: loginOptions ? 'flex' : 'none'}}>
+            <div className="login--option" onClick={handleFacebookLogin}>Facebook</div>
+            <div className="login--option" onClick={handleGithubLogin}>Github</div>
+            <div className="login--option" onClick={handleGoogleLogin}>Google</div>
+          </div>
+        </div>
+      </div>
+      <div className="login-main">
+        <div className="login-main--top">
+          <img className="login-main--top-img" src="https://i.pinimg.com/originals/f5/28/cc/f528cc010d8a9bfcef07d08106976d0f.png" alt=""/>
+          <h1>Whatsapp Clone</h1>
+          <p>Redesign</p>
+        </div>
+      </div>
+      <div className="login-main--content">
+        <img src="https://raw.githubusercontent.com/ErnaneJ/To-Do_AppSvelte/main/.github/images/preview.png" alt="preview"/>
+      </div>
+      <div className="footer">
+        Desenvolvido com ❤️ por <a href="/"> Ernane Ferreira</a>
+      </div>
+    </section>
   );
 };

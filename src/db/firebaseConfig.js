7bc1ffb3957d34acb6c7b71a57b04957588813d8
Ernane Ferreira <ernane.junior25@gmail.com>
2021-10-23 23:26:23 -0300

    firebaseConfig.js

---
+++ src/db/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig ={
+  apiKey: "AIzaSyB_1Z4cPgS6YfOxOFBiKhshsqBGaaJJHJ8",
+  authDomain: "whatsappclone-1e469.firebaseapp.com",
+  projectId: "whatsappclone-1e469",
+  storageBucket: "whatsappclone-1e469.appspot.com",
+  messagingSenderId: "999531167227",
+  appId: "1:999531167227:web:2c2010174c75033aad9f5e"
+};
+
+export default firebaseConfig;
